53307b92da1270e6c42fdda7b79861495e251789
Gregory <taylor.gregory2@gmail.com>
2021-11-27 17:26:02 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const bookList = document.querySelector('#list')
+const showPanel = document.querySelector('#show-panel')
+let listOfBooks = document.querySelectorAll('li')
+console.log(listOfBooks)
+
+function loadBooks(){
+    fetch('http://localhost:3000/books')
+    .then(resp => resp.json())
+    .then(books => {
+        books.forEach(book => {
+            renderBook(book)
+        })
+    })
+}
+
+function renderBook(book){
+    let bookLi = document.createElement('li')
+    bookLi.innerHTML = book.title
+    bookList.appendChild(bookLi)
+}
+
+function renderInfo(book){
+    let bookInfoDiv = document.createElement('div')
+    let bookImg = document.createElement('img')
+    bookImg.src = book.img_url
+    let bookInfoPara = ('p')
+    bookInfoPara.innerHTML = book.description
+    let usernamesOfLikes = book.users.username
+    let bookLikes = document.createElement('li')
+    usernamesOfLikes.forEach(user => {
+        bookLikes.innerHTML = user
+    })
+    let bookInfoArray = [bookImg, bookInfoPara, bookLikes]
+
+    showPanel.appendChild(bookInfoDiv)
+    bookInfoArray.forEach(elem => {
+        bookInfoDiv.appendChild(elem)
+    })
+
+
+}
+
+loadBooks()
